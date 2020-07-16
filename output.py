cfa72bb4cfd1bbdc49f2802871389ef1e6e42b10
ben <benfd@outlook.com>
2020-07-16 15:32:21 +0100

    added app.py and output.py

---
+++ output.py
@@ -0,0 +1,17 @@
+# Dumb functions
+
+def print_menu():
+    print("Options:")
+    print("1: Display All Tasks")
+    print("2: Get Uncompleted Tasks")
+    print("3: Get Completed Tasks")
+    print("4: Mark Task as Complete")
+    print("5: Get Tasks Which Take Longer Than a Given Time")
+    print("6: Find Task by Description")
+    print("7: Add a new Task to list")
+    print("Q or q: Quit")
+
+def print_task(task):
+    print(f'Description: { task["description"] }')
+    print(f'Status: { "Completed" if task["completed"] else "Incomplete"}')
+    print(f'Time Taken: {task["time_taken"]} mins')
