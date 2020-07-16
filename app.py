567b2554654eb078b708a7fc3644124306142e37
ben <benfd@outlook.com>
2020-07-16 16:17:34 +0100

    Broke inputs down to own functions

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 # Import functions
 import modules.task_list as t_list
 import modules.output as dumb
+import data.task_list as data
+import modules.modules.input as user_input
 
 # Main loop
 while (True):
@@ -11,26 +13,21 @@ while (True):
     if (option.lower() == 'q'):
         break
     if option == '1':
-        dumb.print_list(t_list.tasks)
+        dumb.print_list(data.tasks)
     elif option == '2':
-        dumb.print_list(t_list.get_uncompleted_tasks(t_list.tasks))
+        dumb.print_list(t_list.get_uncompleted_tasks(data.tasks))
     elif option == '3':
-        dumb.print_list(t_list.get_completed_tasks(t_list.tasks))
+        dumb.print_list(t_list.get_completed_tasks(data.tasks))
     elif option == '4':
-        description = input("Enter task description to search for: ")
-        task = t_list.get_task_with_description(t_list.tasks, description)
+        task = t_list.get_task_with_description(data.tasks, user_input.desc_input())
         if task != "Task Not Found":
             t_list.mark_task_complete(task)
     elif option == '5':
-        time = int(input("Enter task duration: "))
-        dumb.print_list(t_list.get_tasks_taking_longer_than(t_list.tasks, time))
+        dumb.print_list(t_list.get_tasks_taking_longer_than(data.tasks, user_input.task_time()))
     elif option == '6':
-        description = input("Enter task description to search for: ")
-        print(t_list.get_task_with_description(t_list.tasks, description))
+        print(t_list.get_task_with_description(data.tasks, user_input.desc_input()))
     elif option == '7':
-        description = input("Enter description: ")
-        time_taken = int(input("Enter time taken: "))
-        task = t_list.create_task(description, time_taken)
-        t_list.tasks.append(task)
+        task = t_list.create_task(user_input.desc_input(), user_input.task_time())
+        data.tasks.append(task)
     else:
         print("Invalid Input - choose another option")
---
+++ modules/modules/input.py
@@ -0,0 +1,9 @@
+# Input functions
+
+def task_time():
+    time = int(input("Enter task duration: "))
+    return time
+
+def desc_input():
+    description = input("Enter description: ")
+    return description
