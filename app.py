8b4ba4339af0b46c135150bbd56cd6698e873b44
ben <benfd@outlook.com>
2020-07-16 15:49:37 +0100

    Provided alias's for imports

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 # Main app file
 
 # Import functions
-import task_list
-import output
+import modules.task_list as t_list
+import modules.output as dumb
 
 # Main loop
 while (True):
-    output.print_menu()
+    dumb.print_menu()
     option = input("Select an option 1, 2, 3, 4, 5 or (Q)uit: ")
     if (option.lower() == 'q'):
         break
     if option == '1':
-        output.print_list(task_list.tasks)
+        dumb.print_list(t_list.tasks)
     elif option == '2':
-        output.print_list(task_list.get_uncompleted_tasks(task_list.tasks))
+        dumb.print_list(t_list.get_uncompleted_tasks(t_list.tasks))
     elif option == '3':
-        output.print_list(task_list.get_completed_tasks(task_list.tasks))
+        dumb.print_list(t_list.get_completed_tasks(t_list.tasks))
     elif option == '4':
         description = input("Enter task description to search for: ")
-        task = task_list.get_task_with_description(task_list.tasks, description)
+        task = t_list.get_task_with_description(t_list.tasks, description)
         if task != "Task Not Found":
-            task_list.mark_task_complete(task)
+            t_list.mark_task_complete(task)
     elif option == '5':
         time = int(input("Enter task duration: "))
-        output.print_list(task_list.get_tasks_taking_longer_than(task_list.tasks, time))
+        dumb.print_list(t_list.get_tasks_taking_longer_than(t_list.tasks, time))
     elif option == '6':
         description = input("Enter task description to search for: ")
-        print(task_list.get_task_with_description(task_list.tasks, description))
+        print(t_list.get_task_with_description(t_list.tasks, description))
     elif option == '7':
         description = input("Enter description: ")
         time_taken = int(input("Enter time taken: "))
-        task = task_list.create_task(description, time_taken)
-        tasks.append(task)
+        task = t_list.create_task(description, time_taken)
+        t_list.tasks.append(task)
     else:
         print("Invalid Input - choose another option")
